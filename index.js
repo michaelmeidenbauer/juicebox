71f00f5090fb8dbb8349d4eab0ea8852c3bc3710
michaelmeidenbauer <70448346+michaelmeidenbauer@users.noreply.github.com>
2021-08-19 21:29:24 -0500

    add initial routing

---
+++ index.js
@@ -0,0 +1,18 @@
+const PORT = 3000;
+const express = require('express');
+const volleyball = require('volleyball');
+const server = express();
+const apiRouter = require("./api");
+const bodyParser = require('body-parser');
+const { client } = require('./db');
+
+client.connect();
+server.use(volleyball);
+server.use(bodyParser.json());
+server.use('/api', apiRouter);
+
+
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
